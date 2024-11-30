8ff58698eae1e63266a7a6ee4f11db0ef56a1f93
freelancersifat627e <freelancersifat627@gmail.comm>
2024-11-30 12:15:31 +0600

    compleate header section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,12 +2,37 @@
 import React from "react";
 import "./Header.css";
 import Navbar from "../Navbar/Navbar";
+import coverImg from "../../assets/hero-cover-1.png";
 
 const Header = () => {
   return (
-    <div>
+    <>
       <Navbar />
-    </div>
+      {/* Start Header */}
+      <header className="header">
+        <div className="header-container">
+          {/* Header Text */}
+          <div>
+            <h5 className="header-sub-title">Welcome</h5>
+            <h1 className="header-title">
+              Best Learning <br /> Opportunities
+            </h1>
+            <h4 className="header-description">
+              Our goal is to make online <br /> education work for everyone
+            </h4>
+            {/* button */}
+            <div className="header-btns">
+              <button className="btn-join-us">Join Us</button>
+              <button className="btn-learn-more">Learn More</button>
+            </div>
+          </div>
+          {/* Header Cover Img */}
+          <div>
+            <img src={coverImg} alt="cover img" />
+          </div>
+        </div>
+      </header>
+    </>
   );
 };
 
